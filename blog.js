366dd34be5c7398c49c1f3d0b77e5a67e25041ec
SazWebDesigns <sazwebdesigns@gmail.com>
2017-11-26 01:13:51 +0000

    Added gallery.html and implement loading photos from json placeholder. Added blog.html

--- blog.js
+++ blog.js
@@ -19,6 +19,23 @@ function loadJson(){
    .catch((err) => console.log(err))
 }
 
+function loadPhotos(){
+    fetch("https://jsonplaceholder.typicode.com/photos")
+    .then((res) => res.json())
+    .then((data) => {
+        let output = "<h2>Gallery</h2>";
+        data.forEach(function(photo){
+           output+= `
+           <div class="photo">
+                <h4>${photo.title}</h4>
+                <img src=${photo.url}>
+            </div>`;
+        })
+        document.getElementById('gallery').innerHTML = output;
+   })
+   .catch((err) => console.log(err))
+}
 
 
 loadJson();
+loadPhotos();
